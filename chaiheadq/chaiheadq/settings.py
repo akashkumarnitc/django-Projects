e8ccbfb30175ce5d4bf68ea39a55ab3b1732703a
akashkumarnitc <akash_b220140cs@nitc.ac.in>
2024-10-02 00:36:26 +0530

    Updated for varcel

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i@aqgt4e%68w%pqjjvo#u6m1z-u&(0ct2@6cpxl$id+90bze)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
